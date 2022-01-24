e55e538d07d87b6dfa25d0a209745f539a4fb627
Nekoriee <portalcat13@gmail.com>
2022-01-24 21:12:53 +1000

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-    entry: ['/src/scripts/navbar.js', '/src/scripts/Messenger_lodash.js'],
-output: {
-    path: __dirname + "/src/scripts/dist",
-    filename: 'Messenger_lodash.js',
-  },
+    entry: {
+	Messenger_lodash: ['/src/scripts/navbar.js', '/src/scripts/Messenger_lodash.js'],
+	Messenger: ['/src/scripts/navbar.js', '/src/scripts/Messenger.js'],
+	Countdown: ['/src/scripts/navbar.js', '/src/scripts/Countdown.js'],
+	Game_PilotBrothers: ['/src/scripts/navbar.js', '/src/scripts/Game_PilotBrothers.js'],
+	Game_WhackAMole: ['/src/scripts/navbar.js', '/src/scripts/Game_WhackAMole.js'],
+	navbar: '/src/scripts/navbar.js',
+    },
+
+    output: {
+	filename: '[name].js',
+	path: __dirname + '/src/scripts/dist',
+      },
 };
